090f4a37de6794bd47376299e8fb382a6420d241
79234 <marusya0323@gmail.com>
2023-11-02 22:15:08 +0700

    Преобразования URL-адресов

--- designpro/catalog/urls.py
+++ designpro/catalog/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
-from . import views
 
+from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+                  path('', ViewRequests.as_view(), name='index'),
+                  path('register/', RegistrateUser.as_view(), name='registration'),
+                  path('login/', BBLoginView.as_view(), name='login'),
+                  path('logout/', BBLogoutView.as_view(), name='logout'),
+                  path('request/create', CreateRequest.as_view(), name='request_create'),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
